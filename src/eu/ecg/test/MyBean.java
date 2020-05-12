5452eed6161fa0c1142f2f29486d9b530acd75d5
Daniel Bleisteiner <daniel.bleisteiner@ecg.de>
2020-05-12 12:43:18 +0200

    Problem identifiziert

--- src/eu/ecg/test/MyBean.java
+++ src/eu/ecg/test/MyBean.java
@@ -1,31 +1,68 @@
 package eu.ecg.test;
+
 import java.io.Serializable;
 
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Named
+@ConversationScoped
 public class MyBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private static final String VIEW_ID = "/Transactional.xhtml?faces-redirect=true";
+
     @Inject
     @ApplicationManaged
-    private EntityManager em;
+    private EntityManager em1;
+
+    @Inject
+    @ContainerManaged
+    private EntityManager em2;
+
+    private String value;
 
     @Transactional
-    public void save() {
-        System.out.println("MyBean.save()");
-        this.em.flush(); // NOTE: needs Transaction!
+    public String save1() {
+        System.out.println("MyBean.save1()");
+        this.em1.flush(); // NOTE: needs Transaction!
         System.out.println("OK!");
+        return VIEW_ID;
     }
 
     @Transactional
-    public void select() {
-        System.out.println("MyBean.select()");
-        this.em.flush(); // NOTE: needs Transaction!
+    public String save2() {
+        System.out.println("MyBean.save2()");
+        this.em2.flush(); // NOTE: needs Transaction!
         System.out.println("OK!");
+        return VIEW_ID;
+    }
+
+    @Transactional
+    public String select1() {
+        System.out.println("MyBean.select1()");
+        this.em1.flush(); // NOTE: needs Transaction!
+        System.out.println("OK!");
+        return VIEW_ID;
+    }
+
+    @Transactional
+    public String select2() {
+        System.out.println("MyBean.select2()");
+        this.em2.flush(); // NOTE: needs Transaction!
+        System.out.println("OK!");
+        return VIEW_ID;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 }
